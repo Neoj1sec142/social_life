7cf519746b4174eedb4026b06944f7d5aec8813f
Mark Harmon <neothej1sec@gmail.com>
2022-12-31 17:57:10 -0600

    new pages init

---
+++ frontend/src/components/profile/Emmsisions.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Emmsisions = () => {
+  return (
+    <div>Emmsisions</div>
+  )
+}
+
+export default Emmsisions
---
+++ frontend/src/components/profile/Warming.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Warming = () => {
+  return (
+    <div>Warming</div>
+  )
+}
+
+export default Warming
